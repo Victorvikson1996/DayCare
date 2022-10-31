fc5784847bdfba7955ddbe25c2dd0a1372ae42d0
Victor Ezea <victorvikson@gmail.com>
2022-10-31 14:32:52 +0100

    Project Setup

---
+++ src/components/CheckoutItem.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CheckoutItem = () => {
+  return (
+    <View>
+      <Text>CheckoutItem</Text>
+    </View>
+  );
+};
+
+export default CheckoutItem;
---
+++ src/components/ProductItem.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ProductItem = () => {
+  return (
+    <View>
+      <Text>ProductItem</Text>
+    </View>
+  );
+};
+
+export default ProductItem;
